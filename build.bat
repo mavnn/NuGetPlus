833eb60c12d86e626ee055057bd9d9b697a1fb89
Michael Newton <michael@mavnn.co.uk>
2013-06-24 13:43:57 +0100

    Let's actually build. Oops.
    #ngp-15 Fixed

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-REM tools\Fake\FAKE.exe tools\Core.fsx "tools=tools" "solution=NuGetPlus.sln"
+tools\Fake\FAKE.exe tools\Core.fsx "tools=tools" "solution=NuGetPlus.sln"
 tools\Fake\FAKE.exe .\PackNGP.fsx "tools=tools"
