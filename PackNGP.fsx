a5288eff440da85e8ce2c0de62e28c4170348f3f
Michael Newton <michael@mavnn.co.uk>
2013-06-24 13:40:01 +0100

    Make sure that the NuGetPlus package is currently packaged
    As an executable tool rather than a library.
    
    #ngp-15 fixed

---
+++ PackNGP.fsx
@@ -0,0 +1,30 @@
+#r    "./tools/fake/fakelib.dll"
+open Fake
+open System.IO
+open System.Text.RegularExpressions
+
+let packExe () =
+    let tools = environVar "tools"
+    let versionRegex = Regex(@"AssemblyInformationalVersion\(\""(?<version>.*)\""\)")
+    let version = 
+        ReadFileAsString ("NuGetPlus" @@ "AssemblyInfo.fs")
+        |> versionRegex.Match
+        |> (fun m -> m.Groups.["version"].Value)
+    let setNugetParams param =
+         { param with 
+                    Version = version
+                    Project = "ngp"
+                    OutputPath = "." @@ "output"
+                    WorkingDir = "."
+                    ToolPath = tools @@ "NuGet" @@ "NuGet.exe"
+                    AccessKey = environVarOrDefault "apikey" ""
+                    PublishUrl = environVarOrDefault "pushurl" ""
+                    NoPackageAnalysis = true
+                    Publish = not isLocalBuild }
+    NuGetPack setNugetParams ("NuGetPlus" @@ "ngp.nuspec")
+    if not isLocalBuild then
+        NuGetPublish setNugetParams
+
+let Default = Target "Default" packExe
+
+RunParameterTargetOrDefault "Target" "Default"
--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-tools\Fake\FAKE.exe tools\Core.fsx "tools=tools" "solution=NuGetPlus.sln"
+REM tools\Fake\FAKE.exe tools\Core.fsx "tools=tools" "solution=NuGetPlus.sln"
+tools\Fake\FAKE.exe .\PackNGP.fsx "tools=tools"
